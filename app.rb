32f1a88a429a313b29fef7ca641d21f8d6f90000
Richie <richiejoeyoung@gmail.com>
2017-01-09 19:14:08 -0500

    add resources to webserver

--- app.rb
+++ app.rb
@@ -2,17 +2,17 @@ require 'sinatra'
 require 'yacs_catalog_service/catalog'
 
 get '/sections' do
-
+  json: { sections: Catalog.sections }
 end
 
 get '/courses' do
-
+  json: { courses: Catalog.courses }
 end
 
 get '/departments' do 
-
+  json: { departments: Catalog.departments }
 end
 
 get '/schools' do
-
+  json: { schools: Catalog.schools }
 end
