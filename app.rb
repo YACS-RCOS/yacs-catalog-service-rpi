461c51675183fe31db69a6f3852e9ac477ce2682
Richie <richiejoeyoung@gmail.com>
2017-01-09 19:14:49 -0500

    what is ruby where r my meta?

--- app.rb
+++ app.rb
@@ -1,18 +1,6 @@
 require 'sinatra'
 require 'yacs_catalog_service/catalog'
 
-get '/sections' do
-  json: { sections: Catalog.sections }
-end
-
-get '/courses' do
-  json: { courses: Catalog.courses }
-end
-
-get '/departments' do 
-  json: { departments: Catalog.departments }
-end
-
-get '/schools' do
-  json: { schools: Catalog.schools }
+%w(sections courses departments schools).each do |resource|
+  get "/#{resource}" {json: { resource => Catalog.send(resource) }}
 end
