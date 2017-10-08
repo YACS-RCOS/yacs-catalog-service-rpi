01004f72fed7bed288bc9e0433bf3afb115a25f8
richi <richiejoeyoung@gmail.com>
2017-10-08 22:51:08 +0000

    move hardcoded constants to .env file

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM ruby:2.4.0-alpine
 
 RUN apk add --update ruby-dev build-base
 
-ENV SEMESTER 201709
-
 ENV INSTALL_PATH /usr/src/app/
 RUN mkdir -p $INSTALL_PATH
 WORKDIR $INSTALL_PATH
--- lib/catalog.rb
+++ lib/catalog.rb
@@ -3,13 +3,13 @@ require './lib/catalog/banner_client'
 require './lib/catalog/yaml_client'
 
 module Catalog
-  BANNER_SECTIONS_URI = "https://sis.rpi.edu/reg/rocs/YACS_201709.xml"
-  BANNER_COURSES_URI = "https://sis.rpi.edu/reg/rocs/201709.xml"
-  ACALOG_API_URI = "http://rpi.apis.acalog.com"
-  ACALOG_API_KEY = "3eef8a28f26fb2bcc514e6f1938929a1f9317628"
+  BANNER_SECTIONS_URI = ENV['BANNER_SECTIONS_URI'] # "https://sis.rpi.edu/reg/rocs/YACS_201709.xml"
+  BANNER_COURSES_URI = ENV['BANNER_COURSES_URI'] # "https://sis.rpi.edu/reg/rocs/201709.xml"
+  ACALOG_API_URI = ENV['ACALOG_API_URI'] # "http://rpi.apis.acalog.com"
+  ACALOG_API_KEY = ENV['ACALOG_API_KEY'] # "3eef8a28f26fb2bcc514e6f1938929a1f9317628"
 
   class Aggregator
-    def initialize 
+    def initialize
       @yaml_client = YamlClient.new
       @banner_client = BannerClient.new BANNER_COURSES_URI, BANNER_SECTIONS_URI
       @acalog_client = AcalogClient.new ACALOG_API_URI, ACALOG_API_KEY
@@ -18,6 +18,10 @@ module Catalog
 
     def sections 
       @banner_client.sections
+    end
+    
+    def section_seats
+      @banner_client.section_seats
     end
 
     def courses
